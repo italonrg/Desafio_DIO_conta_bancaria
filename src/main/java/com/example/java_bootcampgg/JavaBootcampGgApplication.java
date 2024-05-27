ba56034fc9d8656349deba8cf921476f70c52b88
italonrg <italonrg500@gmail.com>
2024-05-27 15:49:29 -0300

    primeiro Commit

---
+++ src/main/java/com/example/java_bootcampgg/JavaBootcampGgApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_bootcampgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootcampGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootcampGgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_bootcampgg/JavaBootcampGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_bootcampgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootcampGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
